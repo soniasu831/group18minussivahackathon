f80215f9a55a9b5b53ff9c389db50bcabe5fb3d5
louise <louise.furong@gmail.com>
2025-10-18 05:29:01 -0400

    initial commit for calculating ppk process index

---
+++ process_indices.R
@@ -0,0 +1,65 @@
+# %%
+
+#' Process Performance Index (for skewed, uncentered data)
+#' @input mu, process mean
+#' @input sigma_t, process standard deviation
+#' @input lower, lower specification limit
+#' @input upper, upper specification limit
+ppk = function(mu, sigma_t, lower = NULL, upper = NULL){
+  if(!is.null(lower)){
+    a = abs(mu - lower) / (3 * sigma_t)
+  }
+  if(!is.null(upper)){
+    b = abs(upper - mu) /  (3 * sigma_t)
+  }
+  # We can also write if else statements like this
+  # If we got both stats, return the min!
+  if(!is.null(lower) & !is.null(upper)){
+    min(a,b) %>% return()
+    
+    # If we got just the upper stat, return b (for upper)
+  }else if(is.null(lower)){ return(b) 
+    
+    # If we got just the lower stat, return a (for lower)
+  }else if(is.null(upper)){ return(a) }
+}
+
+
+
+
+# %%
+
+#' Function to calculate Ppks on a per-solar panel level
+#' @import dplyr
+getPpks = function(panel_data){
+
+    nominal_eff = tibble(
+        m = 0.20,
+        p = 0.17,
+        t = 0.12
+    )
+
+    indices = panel_data %>% group_by(panel_id, panel_type) %>% reframe(
+        mu = mean(eff),
+        sigma_t = sd(eff),
+    ) %>% reframe(
+        panel_id = panel_id,
+        ppk = case_when(
+            panel_type == "M" ~ ppk(mu, sigma_t, nominal_eff$m),
+            panel_type == "P" ~ ppk(mu, sigma_t, nominal_eff$p),
+            panel_type == "T" ~ ppk(mu, sigma_t, nominal_eff$t)
+        )
+    )
+
+    return(indices)
+
+}
+
+# %%
+
+library(readr)
+library(tidyverse)
+
+panel_data = read_csv("louise.csv")
+
+indices = getPpks(panel_data)
